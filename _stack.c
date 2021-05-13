8dd1bdd6608cbc62d7c1df167d62dc1f3cb76ab1
Alex Rivera <alexriveracruz4@gmail.com>
2021-05-13 16:23:45 +0000

    [ADD] stack opcode

---
+++ _stack.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * _stack - sets the format of the data to a stack (LIFO)
+ * @stack: pointer to pointer to stack
+ * @line_number: number of line executed
+ *
+ * Return: void
+ */
+void _stack(stack_t **stack, unsigned int line_number)
+{
+	(void) line_number;
+	(void) stack;
+
+	flag.mode = 0;
+}
